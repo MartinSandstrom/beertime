9c462f29111305c4b918842d9052cc907a4a6000
Martin Sandström <maartiin_91@hotmail.com>
2016-05-21 17:26:08 +0200

    Adding some style and login redirects

--- app/js/scoreboard.js
+++ app/js/scoreboard.js
@@ -1,7 +1,13 @@
 
-angular.module('beerTime').controller('ScoreBoardCtrl', ['$scope', '$firebaseArray', function($scope, $firebaseArray) {
+angular.module('beerTime').controller('ScoreBoardCtrl', ['$scope', '$firebaseArray', '$location', function($scope, $firebaseArray, $location) {
 
 	var ref = new Firebase('https://beertime.firebaseio.com/data');
+
+	var auth = ref.getAuth();
+	if(!auth) {
+		$location.path('/');
+	}
+
 	$scope.messages = $firebaseArray(ref);
-	
+
 }]);
