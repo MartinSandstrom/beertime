49331d15eda4dc879f3768e0601dfabca774d996
Martin Sandström <maartiin_91@hotmail.com>
2016-05-23 11:50:19 +0200

    Adding style to login buttons

--- app/js/app.js
+++ app/js/app.js
@@ -1,20 +1,24 @@
 
 angular.module('beerTime', ['firebase', 'ngRoute'])
-  .config(function ($routeProvider) {
-      $routeProvider
-      .when('/', {
-          templateUrl: 'views/landingpage.html',
-          controller: ''
-      })
-      .when('/scoreboard', {
-          templateUrl: 'views/scoreboard.html',
-          controller: 'ScoreBoardCtrl'
-      })
-			.when('/beer', {
-				templateUrl: 'views/beers.html',
-				controller: 'BeerCtrl'
-			})
-      .otherwise({
-          redirectTo: '/'
-      });
-  })
+.config(function ($routeProvider) {
+	$routeProvider
+	.when('/', {
+		templateUrl: 'views/landingpage.html',
+		controller: ''
+	})
+	.when('/scoreboard', {
+		templateUrl: 'views/scoreboard.html',
+		controller: 'ScoreBoardCtrl'
+	})
+	.when('/beer', {
+		templateUrl: 'views/beers.html',
+		controller: 'BeerCtrl'
+	})
+	.when('/login', {
+		templateUrl: 'views/login.html',
+		controller: 'LoginCtrl'
+	})
+	.otherwise({
+		redirectTo: '/'
+	});
+})
