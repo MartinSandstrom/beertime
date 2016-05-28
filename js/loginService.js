6dd377b30f4bb14dc4862390c25690c6d8230b8f
Martin Sandström <maartiin_91@hotmail.com>
2016-05-28 13:46:06 +0200

    Update 2016-05-28T11:46:04.869Z

--- js/loginService.js
+++ js/loginService.js
@@ -3,6 +3,16 @@ angular.module('beerTime').factory('LoginService', ['$location', function($locat
 		logInPrompt: function(ref, provider) {
 			return ref.authWithOAuthPopup(provider, function(error, authData) {
 				if (error) {
+					this.logInRedirect(ref, provider);
+					console.log("Login Failed!", error);
+				} else {
+					$location.path('beer');
+				}
+			});
+		},
+		logInRedirect: function(ref, provider) {
+			ref.authWithOAuthRedirect(provider, function(error, authData) {
+				if (error) {
 					console.log("Login Failed!", error);
 				} else {
 					$location.path('beer');
